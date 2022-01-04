1005eb18407437e85283f7e817341483bfae2fb9
sjani <shaunak.jani@move.com>
2022-01-04 15:39:14 -0800

    Initial commit

---
+++ app/src/main/java/com/sjani/rdc_android_interview_sandbox/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sjani.rdc_android_interview_sandbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
